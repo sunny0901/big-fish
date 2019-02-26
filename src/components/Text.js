a11cdfde78882f527814ae7a57be7205ef907ae6
Yifan Sun <sunny_yfs@hotmail.com>
2019-02-25 21:23:13 -0500

    onblur first

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './styles/Text.css'
 
-export default function Text({type, children}){
+export default function Text({type, children}){ //deconstruction when pass in props
     return <p className = {'text_base' + type}>{children}</p>
 };
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -4,20 +4,32 @@ import Text from './Text'
 
 export default class TextInput extends Component {
 
+    state = {
+        errMes: '',
+    }
+
+    _onBlur = ({target: {value}}) =>{ //pass in event
+        console.log('on Blur', value); //***event.target.value
+        if (!value) {
+            this.setState({errMes: 'Required'});  //this is pointed to global now
+        }
+    }
+
     render() {
+
         const {
             style,
             placeholder,
         } = this.props;
 
         return (
-            <div style = {{...styles.container, ...style}}>
+            <div style = {{...styles.container, ...style}}> 
                 <div>
-                    <input placeholder = {placeholder} style = {styles.input}/>
+                    <input onBlur = {this._onBlur} placeholder = {placeholder} style = {styles.input}/>
                     <div style = {styles.line}
                     ></div>
                 </div>
-                <Text type = 'xs err'>error</Text>
+                <Text type = 'xs err'>{this.state.errMes}</Text>
             </div>
         );
     }
