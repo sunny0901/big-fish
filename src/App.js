1ee9db6922982d7d1d91aa94e3cc13d3e2da7824
Yifan Sun <sunny_yfs@hotmail.com>
2019-03-04 16:33:20 -0500

    react-router first

--- src/App.js
+++ src/App.js
@@ -1,79 +1,16 @@
-import React, { Component } from 'react';
-import styles from './styles/App'
-import TextInput from './components/TextInput'
-import Button from './components/Button'
-import validate, {
-  isExist,
-  emailFormat,
-  uppercase,
-  lowercase,
-  passwordLength,
-  nameLength,
-} from './utils/validations'
+import React, { Component} from 'react';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import {Route} from 'react-router-dom'
 
 class App extends Component {
-
-  state = {
-    emailErr: '',
-    passwordErr: '',
-    nameErr: '',
-  }
-
-  input_value = {
-    email: '',
-    password: '',
-    name: ''
-  }
-
-  onBlur = ({target: {id, value}}) => { //pass in event
-    //console.log('on Blur', value); //***event.target.value
-    if (!value) {
-      this.setState ({
-        [id + 'Err']: 'Required'
-      });
-    }
-  }
-
-  onChange = ({target: {id, value}}) =>{
-    this.input_value[id] = value
-    if (value) {
-      this.setState ({
-        [id + 'Err']: ''
-      });
-    }
-  }
-
-  onSubmit = () =>{
-    const emailErrMes = validate([isExist, emailFormat], this.input_value.email)
-    const passErrMes = validate([isExist, passwordLength, uppercase, lowercase], this.input_value.password)
-    const nameErrMes = validate([nameLength], this.input_value.name)
-
-    if (emailErrMes || passErrMes || nameErrMes){
-      this.setState({
-        emailErr: emailErrMes,
-        passwordErr: passErrMes,
-        nameErr: nameErrMes,
-      })
-    }
-  }
-  
+ 
   render() {
     return (
-      <div style={styles.container}>
-        <div style={styles.panel}>
-          <p style={styles.logo}>BIG FISH</p>
-          <TextInput id={'email'} onBlur={this.onBlur} onChange={this.onChange} errMes={this.state.emailErr} style={{marginBottom: 8}} placeholder="Email" />
-          <TextInput id={'password'} onBlur={this.onBlur} onChange={this.onChange} errMes={this.state.passwordErr} style={{marginBottom: 8}} placeholder="Password" />
-          <TextInput id={'name'} onBlur={this.onBlur} onChange={this.onChange} errMes={this.state.nameErr} style={{marginBottom: 73}} placeholder="Name" />
-          <Button onClick={this.onSubmit} style={{marginBottom: 52}} btnText='Signup' />
-          <div style={styles.footer_container}>
-            <div style={styles.footer}>
-              <p style={styles.footer_text}>Already have an account?&nbsp;&nbsp;</p>
-              <p style={styles.footer_login}>Login</p>
-            </div>
-          </div>
-        </div>
-      </div>
+        <>
+        <Route path = '/Signup' component = {Signup} />
+        <Route path = '/Login' component = {Login} />
+        </>
     );
   }
 }
---
+++ src/pages/Signup.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import styles from '../styles/App'
+import TextInput from '../components/TextInput'
+import Button from '../components/Button'
+import { Link } from 'react-router-dom'
+import validate, {
+  isExist,
+  emailFormat,
+  uppercase,
+  lowercase,
+  passwordLength,
+  nameLength,
+} from '../utils/validations'
+
+class Signup extends Component {
+
+  state = {
+    emailErr: '',
+    passwordErr: '',
+    nameErr: '',
+  }
+
+  input_value = {
+    email: '',
+    password: '',
+    name: ''
+  }
+
+  onBlur = ({target: {id, value}}) => { //pass in event
+    //console.log('on Blur', value); //***event.target.value
+    if (!value) {
+      this.setState ({
+        [id + 'Err']: 'Required'
+      });
+    }
+  }
+
+  onChange = ({target: {id, value}}) =>{
+    this.input_value[id] = value
+    if (value) {
+      this.setState ({
+        [id + 'Err']: ''
+      });
+    }
+  }
+
+  onSubmit = () =>{
+    const emailErrMes = validate([isExist, emailFormat], this.input_value.email)
+    const passErrMes = validate([isExist, passwordLength, uppercase, lowercase], this.input_value.password)
+    const nameErrMes = validate([nameLength], this.input_value.name)
+
+    if (emailErrMes || passErrMes || nameErrMes){
+      this.setState({
+        emailErr: emailErrMes,
+        passwordErr: passErrMes,
+        nameErr: nameErrMes,
+      })
+    }
+  }
+  
+  render() {
+    return (
+      <div style={styles.container}>
+        <div style={styles.panel}>
+          <p style={styles.logo}>BIG FISH</p>
+          <TextInput id={'email'} onBlur={this.onBlur} onChange={this.onChange} errMes={this.state.emailErr} style={{marginBottom: 8}} placeholder="Email" />
+          <TextInput id={'password'} onBlur={this.onBlur} onChange={this.onChange} errMes={this.state.passwordErr} style={{marginBottom: 8}} placeholder="Password" />
+          <TextInput id={'name'} onBlur={this.onBlur} onChange={this.onChange} errMes={this.state.nameErr} style={{marginBottom: 73}} placeholder="Name" />
+          <Button onClick={this.onSubmit} style={{marginBottom: 52}} btnText='Signup' />
+          <div style={styles.footer_container}>
+            <div style={styles.footer}>
+              <p style={styles.footer_text}>Already have an account?&nbsp;&nbsp;</p>
+              <Link to = './Login'><p style={styles.footer_login}>Login</p></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signup;
