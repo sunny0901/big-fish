4d76cdbf40420ec3536cec8cbc32601b0a8c8b05
Yifan Sun <yifan.sunny@outlook.com>
2019-10-21 15:42:05 -0400

    show question in answer page

--- src/pages/Answers.js
+++ src/pages/Answers.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Question from '../components/Question'
 import List from '../components/List'
 import { connect } from 'react-redux'
+import question_style from './styles/Questions'
 
 class Answers extends Component {
 
@@ -10,17 +11,25 @@ class Answers extends Component {
         this.props.getAllAnswers(id);
     }
     render() {
+        const { question, answers } = this.props;
         return (
             <>
-                <Question />
-                {/* <List data={answers} renderRow={ answer => 
-                <div>{ answer }</div> } /> */}
+                <div style={question_style.panel}>
+                    {question && <Question title={question.title}
+                        content={question.content}
+                        id={question.id} />}
+                </div>
+                <div style={question_style.panel}>
+                    <List data={answers} renderRow={answer =>
+                        <div>{answer.content}</div>} />
+                </div>
             </>
         )
     }
 }
 
 const mapState = (state, ownProps) => ({
+    question: state.questions.find((q) => q.id == ownProps.match.params.id),
     answers: state.answers[ownProps.match.params.id]
 });
 
